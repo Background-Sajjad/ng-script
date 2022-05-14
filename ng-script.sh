e7e0f217b1bb5952cf3fd4df26047f5905817e34
Sajjad <63840882+Background-Sajjad@users.noreply.github.com>
2022-05-14 19:45:00 +0600

    Add files via upload

--- ng-script.sh
+++ ng-script.sh
@@ -4,9 +4,8 @@ sleep 3
 
 cd
 rm -rf ngrok
-cd /sdcard
-cp -rf ngrok $HOME
-cd
+wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-arm.tgz
+tar -zxvf ngrok-v3-stable-linux-arm.tgz
 chmod +x ngrok
 ./ngrok config add-authtoken 1aZKC0rOP25LxKqkcuAD31VDPVu_7LCEgP6EtdcQkTG3zvS7Q
 ./ngrok http 8080
